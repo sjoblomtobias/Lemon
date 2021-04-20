a1fe74c61177eac8fc65f64181492de11de95efe
Tobias Sjöblom <tobiassjoblom16@gmail.com>
2021-04-20 16:46:34 +0200

    Initial commit

---
+++ src/main/java/com/murengezi/lemon/Events/Join.java
@@ -0,0 +1,18 @@
+package com.murengezi.lemon.Events;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+/**
+ * @author Tobias Sjöblom
+ * Created on 2021-04-20 at 16:43
+ */
+public class Join implements Listener {
+
+	@EventHandler
+	public void call(PlayerJoinEvent event) {
+
+	}
+
+}
---
+++ src/main/java/com/murengezi/lemon/Lemon.java
@@ -0,0 +1,17 @@
+package com.murengezi.lemon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Lemon extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+
+	}
+
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+}
